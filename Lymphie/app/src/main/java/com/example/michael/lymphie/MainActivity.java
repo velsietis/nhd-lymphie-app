e793b4dc6b514bcc9b37f874e9c8c0be0e5a7acb
quantumentagle <suneil.tagore@gmail.com>
2018-01-27 17:36:27 +0000

    add camera functionality

--- Lymphie/app/src/main/java/com/example/michael/lymphie/MainActivity.java
+++ Lymphie/app/src/main/java/com/example/michael/lymphie/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.michael.lymphie;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        //startActivity(intent);
     }
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void takePhoto(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    private boolean safeCameraOpen() {
+        boolean qOpened = false;
+
+        try {
+            releaseCameraAndPreview();
+            mCamera = Camera.open();
+            qOpened = (mCamera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+
+    }
+
 }
